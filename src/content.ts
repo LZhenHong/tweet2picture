9fc8529e4aa4b493a6dbd988afbbf059630ef3ae
Eden <levine5blue@gmail.com>
2025-02-20 21:10:27 +0800

    chore: update manifest and remove unused scripts.

--- src/content.ts
+++
@@ -1,3 +0,0 @@
-console.log('Content script loaded');
-
-// 在这里添加将 tweet 转成图片的逻辑
--- src/popup.ts
+++
@@ -1,4 +0,0 @@
-document.getElementById('convert')?.addEventListener('click', () => {
-    console.log('Convert button clicked');
-    // 在这里添加将 tweet 转成图片的逻辑
-});
